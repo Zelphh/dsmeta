53715bb19f49ccda5ce7c507602685ea9f5e99bd
Zelph <matheusmelle2006@gmail.com>
2022-09-09 14:58:32 -0300

    Security config

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 
 function SalesCard() {
 
-    const min = new Date(new Date().setDate(new Date().getDate() - 365));
-    const max = new Date();
+    const min = new Date(new Date().setDate(new Date().getDate() - 365))
+    const max = new Date()
 
-    const [minDate, setMinDate] = useState(min);
+    const [minDate, setMinDate] = useState(min);    
     const [maxDate, setMaxDate] = useState(max);
 
     return (
